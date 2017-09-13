a07d41c6c02cb1f04c9136c3eacec09f8931886b
BivinMac5 <bivinmac5@gmail.com>
2017-09-13 07:02:57 +0530

    D3: First Commit for D3

---
+++ leaveAbsenceApp/src/pages/home/home.ts
@@ -0,0 +1,32 @@
+import { Component } from '@angular/core';
+import { NavController } from 'ionic-angular';
+
+import { SMS } from '@ionic-native/sms';
+
+@Component({
+  selector: 'page-home',
+  templateUrl: 'home.html'
+})
+export class HomePage {
+
+  constructor(public navCtrl: NavController, private smsVar: SMS) {
+
+  }
+
+  sendSMS(){
+    var options={
+      replaceLineBreaks: false, // true to replace \n by a new line, false by default
+      android: {
+        intent: ''
+        //intent: '' // Sends sms without opening default sms app
+      }
+    }
+    this.smsVar.send('416123456', 'Hello Mom!',options)
+      .then(()=>{
+        alert("success");
+      },()=>{
+        alert("failed");
+      });
+  }
+
+}
